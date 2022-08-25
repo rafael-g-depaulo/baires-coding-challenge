53d48173dcadac738cb5a363648acdb34b8c594f
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2022-08-24 23:56:04 -0300

    test

--- packages/apps/client/src/app/Home.tsx
+++ packages/apps/client/src/app/Home.tsx
@@ -1,9 +1,11 @@
+import { api } from "../api"
 import { useHello } from "../api/hello"
 import { environment } from "../environments/environment"
 
 export const Home = () => {
   const { data } = useHello()
 
+  console.log({ environment }, api)
   return (
     <div>
       <p>Message recieved from backend (URL: {environment.apiUrl}) is:</p>
